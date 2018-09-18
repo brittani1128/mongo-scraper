9e011947f03c8a60f1dded1cdf71fff4fb21a7d5
Brittani <brittani1128@gmail.com>
2018-09-17 23:14:11 -0500

    rendering html, error in scraping

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+// var instance = M.Carousel.init({
+//     fullWidth: true
+//   });
+
+  // Or with jQuery
+
+  
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+     
+
+  // function displayResults(data){
+  //   $(".article-results").empty;
+  //   data.forEach(function(article){
+  //     console.log(article);
+
+  //     // var cardDiv = $("<div class='card'>");
+  //     // var title = $("<div class='card-title'>").text(data[i].title)
+  //   })
+  // }
+
+  // Receives json response from query to db in app.get("/all")
+  $.getJSON("/all", function(data){
+    $(".article-results").empty();
+    data.forEach(function(article){
+     
+    var cardDiv = $("<div class='card'>").append(
+      $("<div class='card-title'>").text(article.title),
+      $("<div class='card-image'>").html("<img src='" + article.imageUrl + "'>"),
+      $("<div class='card-content'>").text(article.summary).append(article.link)
+
+    );
+
+    $(".article-results").append(cardDiv);
+     
+    });
+    
+  })
---
+++ server.js
@@ -0,0 +1,108 @@
+
+//TO DO
+
+//Currently: scraping site, storing info in db, getting results from db in json format
+
+//Need to populate html with scraped data results
+//Save button option to save article (adds saved attribute)
+//  Create saved articles route to display all saved articles
+
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var exphbs = require("express-handlebars");
+var path = require("path");
+
+// Initialize Express
+var app = express();
+app.use(express.static("public"));
+
+// Database configuration
+var databaseUrl = "hwScraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+
+//ROUTES =============================================
+
+// Main route (simple Hello World Message); gets overridden by setting express.static(public)
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+
+// Retrieves data from scrapedData collection
+app.get("/all", function(req,res){
+  db.scrapedData.find({}, function(error, found){
+    if (error){
+      console.log(error);
+    }
+    else {
+        //sends data response in json
+        res.json(found);
+    }
+  });
+});
+
+
+// Scrapes website for articles and adds to scrapedData collection
+app.get("/scrape", function(req,res){
+
+  request("https://theonion.com/", function(error, response, html){
+    var $ = cheerio.load(html);
+    
+    var articleBlock = $(".curation-module__item__wrapper");
+    $(articleBlock).each(function(i, element) {
+
+      var imageUrl = $(element).children(".image-container-wrapper").children(".image-container").children("a").children("img").attr("data-src");
+      var title = $(element).children(".content-wrapper").children(".content-meta__headline").children(".content-meta__headline__wrapper").children("h6").text();
+      var summary = $(element).children(".content-meta__excerpt").text();
+      var link = $(element).attr("data-permalink");
+
+      
+    
+    
+      db.scrapedData.insert({title:title, link:link, imageUrl:imageUrl, summary:summary, saved:false});
+      
+    });
+
+  });
+
+});
+
+app.get("/saved", function(req, res){
+    db.scrapedData.find().sort({_id:1}, function(error, data){
+        if (error){
+            console.log(error)
+          }
+          else {
+            res.json(data);
+          }
+    })
+})
+
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
